0eb607d23ac16e2ea27a8dc173e17b215495663b
gabriel dreux <dreuxgabriel@hotmail.com>
2023-07-28 17:09:34 -0300

    alertas + abre/fecha carrinho

--- js/app.js
+++ js/app.js
@@ -110,6 +110,8 @@ cardapio.metodos = {
                     MEU_CARRINHO.push(item[0]);
                 }
 
+
+                cardapio.metodos.mensagem('Item adicionado ao carrinho', 'green');
                 $("#qntd-" + id).text(0);
 
                 cardapio.metodos.atualizarBadgeTotal();
@@ -137,6 +139,38 @@ cardapio.metodos = {
 
         $(".badge-total-carrinho").html(total);
 
+    },
+
+
+
+    abrirCarrinho: (abrir) => {
+
+        if (abrir) {
+            $("#modalCarrinho").removeClass('hidden');
+        } else {
+            $("#modalCarrinho").addClass('hidden');
+        }
+
+
+    },
+
+
+    mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+        let id = Math.floor(Date.now() * Math.random()).toString();
+        
+        let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+        $("#container-mensagens").append(msg);
+
+        setTimeout(() => {
+            $("#msg-" + id).removeClass('fadeInDown');
+            $("#msg-" + id).addClass('fadeOutUp');
+            setTimeout(() => {
+                $("#msg-" + id).remove();
+            }, 800)
+        }, tempo)
+
+        
     },
 
 }
